f1bbdf9fdc33bbe1d7646c45bac69688b9eff614
Vesa Meskanen <vesa.meskanen@logica.com>
2016-08-24 08:48:05 +0300

    Fix jslint errors

--- import.js
+++ import.js
@@ -5,4 +5,4 @@
 var importPipeline = require( './lib/importPipeline' );
 var argv = require('minimist')(process.argv.slice(2)); // -d datapath/
 
-importPipeline.create( argv['d'] );
+importPipeline.create( argv.d );
